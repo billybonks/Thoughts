12c2d37686d48d400ddbab827e79cfebfbb0337e
Sebatien Stettler <sebastienstettler@gmail.com>
2013-11-25 23:36:41 +0200

    [Enhancement] Streamlined Deployment

--- Client/Gruntfile.js
+++ Client/Gruntfile.js
@@ -15,14 +15,27 @@ module.exports = function(grunt) {
         },
 
       }
+    },
+    concat: {
+      options: {
+        separator: '\n'
+      },
+      emberApp: {
+        src:['Scripts/*.js','Scripts/Controllers/*.js','Scripts/Views/*.js','Scripts/Models/*.js','Scripts/Components/*.js'],
+        dest: '../Deploy/emberApp.js'
+      },
+      css:{
+        src:['Content/*.css'],
+        dest:'../Deploy/style.css'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-ember-templates');
-  //grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   //grunt.loadNpmTasks 'grunt-contrib-jshint'
   //grunt.loadNpmTasks 'grunt-contrib-less'
   //grunt.loadNpmTasks 'grunt-contrib-copy'
-  grunt.registerTask('default', ['emberTemplates']);
+  grunt.registerTask('default', ['emberTemplates','concat']);
 
 };
