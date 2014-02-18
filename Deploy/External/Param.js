eba13fc62095006634af05a6576d1db1b08e232e
Sebatien Stettler <sebastienstettler@gmail.com>
2014-02-18 17:46:15 +0200

    added deployment files

---
+++ Deploy/External/Param.js
@@ -0,0 +1,8 @@
+﻿$.urlParam = function (name) {
+    var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results) {
+        return results[1]
+    } else {
+        return null;
+    }
+}
