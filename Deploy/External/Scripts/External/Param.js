228ebc02b29086ce20152c9edfc249dedf4e549e
Sebatien Stettler <sebastienstettler@gmail.com>
2014-03-06 00:51:47 +0200

    Cleaning and ignoring generated files

--- Deploy/External/Scripts/External/Param.js
+++
@@ -1,8 +0,0 @@
-﻿$.urlParam = function (name) {
-    var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
-    if (results) {
-        return results[1]
-    } else {
-        return null;
-    }
-}
