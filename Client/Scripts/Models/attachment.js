10acba111387e7e707ae071eac9be5e1e5b7f00f
Sebatien Stettler <sebastienstettler@gmail.com>
2014-04-22 23:09:58 +0200

    Cleaner

--- Client/Scripts/Models/attachment.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-App.Attachment = DS.Model.extend({
-  data:DS.attr(),
-  sectionid:DS.attr(),
-  tags:DS.hasMany('tag'),
-  type:DS.attr('string')
-})
--- Client/Scripts/Models/tag.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-App.Tag = DS.Model.extend({
-  title: DS.attr('string')
-});
