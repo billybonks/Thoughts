0a7fc7e088060494b69b155c147e6f595daca840
Sebatien Stettler <sebastienstettler@gmail.com>
2014-04-29 00:04:21 +0200

    new Modal framework

--- Client/Plugins/Properties/PropertiesMainComponent.js
+++ Client/Plugins/Properties/PropertiesMainComponent.js
@@ -1,12 +1,20 @@
 'use strict';
 App.PropertiesMainComponent = App.BaseSectionComponent.extend({
+  types:['string','number'],
   didInsertElement:function(){
     console.log(this.toString());
   },
   actions:{
-    openModal:function(modalName,model,secondaryModel){
-      return this.get('targetObject').openModal(modalName,model,secondaryModel);
-    }
+    openModal:function(){
+      var data = {model:{name:null,value:null,type:null},types:this.get('types')};
+      var view = Ember.View.extend({
+        templateName:'propertyForm',
+      });
+      return this.OpenModal( this.get('CreateProperty'),null,data,view,'New Property');
+    },
+  },
+  CreateProperty:function(data){
+    this.submitAttachment(data.model);
   },
   context:function(){
     return this
--- Client/Scripts/Components/BaseSectionComponent.js
+++ Client/Scripts/Components/BaseSectionComponent.js
@@ -2,8 +2,36 @@
 App.BaseSectionComponent = Ember.Component.extend(App.SubmitAttachmentMixin,{
   section:null,
   isEditing:false,
+  modal:null,
+  cancel:null,
+  confirm:null,
+  actions:{
+    BaseModalConfirm:function(){
+      if(this.get('confirm')){
+        this.get('confirm').call(this,this.get('modal.content'));
+      }
+    },
+    BaseModalCancel:function(){
+      if(this.get('cancel')){
+        this.get('cancel').call(this,this.get('modal.content'));
+      }
+    },
+  },
   Notify:function(message,level){
     this.send('notification',message,level);
     this.get('targetObject').FowardNotification(message,level)
+  },
+  OpenModal:function(confirm,cancel,content,contentViewClass,headerText){
+    this.set('confirm',confirm);
+    this.set('cancel',cancel);
+    this.set('modal',Ember.Widgets.ModalComponent.popup({
+      targetObject: this,
+      confirm: "BaseModalConfirm",
+      cancel: "BaseModalCancel",
+      content: content,
+      contentViewClass:contentViewClass,
+      headerText:headerText
+    }));
+    return this.get('modal');
   }
 });
