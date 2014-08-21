0a67897ad5a624d18f9fefee13117ad3068ecb25
Sebatien Stettler <sebastienstettler@gmail.com>
2014-08-21 13:07:35 +0200

    [CLEANUP] Properties now use property-listitem component

---
+++ Client/Plugins/Properties/PropertiesListitem.js
@@ -0,0 +1,21 @@
+'use strict'
+App.PropertiesListItemComponenet = Ember.Component.extend(App.SubmitAttachmentMixin, DragNDrop.DragAndDroppable, {
+  isEditing: false,
+  actions: {
+    toggleEdit: function() {
+      this.get('isEditing') ? this.set('isEditing', false) : this.set('isEditing', true);
+    },
+    Update: function() {
+      this.SaveAttachment(this.get('model'));
+      this.get('isEditing') ? this.set('isEditing', false) : this.set('isEditing', true);
+    }
+  },
+  dragStart: function(event) {
+    console.log('dragStart');
+    var model = this.get('model')
+    var dataTransfer = event.originalEvent.dataTransfer;
+    dataTransfer.setData('Type', 'Attachment');
+    dataTransfer.setData('AttachmentType', 'Property');
+    dataTransfer.setData('id', model.get('id'));
+  }
+})
