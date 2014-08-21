6f93e98d9c4a3dbe7a0bc3a902824169d07a6321
Sebatien Stettler <sebastienstettler@gmail.com>
2014-08-21 16:44:54 +0200

    [CLEANUP] Decoupled basesectioncomponent and open modal

--- Client/Scripts/Mixins/PopupMixin.js
+++ Client/Scripts/Mixins/PopupMixin.js
@@ -1,7 +1,30 @@
 App.PopupMixin = Ember.Mixin.create({
+  modal:null,
+  cancel:null,
+  confirm:null,
   actions:{
-    close: function() {
-      return this.send('closeModal');
-    }
+    BaseModalConfirm:function(){
+      if(this.get('confirm')){
+        this.get('confirm').call(this,this.get('modal.content'));
+      }
+    },
+    BaseModalCancel:function(){
+      if(this.get('cancel')){
+        this.get('cancel').call(this,this.get('modal.content'));
+      }
+    },
+  },
+  OpenModal:function(confirm,cancel,content,contentViewClass,headerText){
+    this.set('confirm',confirm);
+    this.set('cancel',cancel);
+    this.set('modal',Ember.Widgets.ModalComponent.popup({
+      targetObject: this,
+      confirm: "BaseModalConfirm",
+      cancel: "BaseModalCancel",
+      content: content,
+      contentViewClass:contentViewClass,
+      headerText:headerText
+    }));
+    return this.get('modal');
   }
 });
