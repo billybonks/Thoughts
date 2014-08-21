9e75f19cb8250d77ffd110dbbf3099e447c8633b
Sebatien Stettler <sebastienstettler@gmail.com>
2014-08-21 11:24:11 +0200

    [BUGFIX] Made component drag and droppable

--- Client/Plugins/List/ListItemComponent.js
+++ Client/Plugins/List/ListItemComponent.js
@@ -1,5 +1,5 @@
 'use strict';
-App.ListItemComponent = Ember.Component.extend(App.SubmitAttachmentMixin, {
+App.ListItemComponent = Ember.Component.extend(App.SubmitAttachmentMixin,DragNDrop.DragAndDroppable, {
   isEditing: false,
   actions: {
     Edit: function() {
@@ -10,5 +10,13 @@ App.ListItemComponent = Ember.Component.extend(App.SubmitAttachmentMixin, {
       this.get('isEditing') ? this.set('isEditing', false) : this.set('isEditing', true);
       this.SaveAttachment(this.get('model'));
     }
+  },
+  dragStart: function(event) {
+    console.log('dragStart');
+    var model = this.get('model')
+    var dataTransfer = event.originalEvent.dataTransfer;
+    dataTransfer.setData('Type', 'Attachment');
+    dataTransfer.setData('AttachmentType', 'link');
+    dataTransfer.setData('id', model.get('id'));
   }
 })
