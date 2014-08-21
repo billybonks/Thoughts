4840f13c65fb3a48d8e435d454f5a30e495bbbc6
Sebatien Stettler <sebastienstettler@gmail.com>
2014-08-22 00:38:46 +0200

    [BUGFIX] SPring cleaning broke login

--- Client/Scripts/Controllers/ApplicationController.js
+++ Client/Scripts/Controllers/ApplicationController.js
@@ -1,61 +1,69 @@
 'use strict';
-App.ApplicationController = Ember.Controller.extend(App.PopupOpenerMixin,{
-    currentDrag: null,
-    menuOpen:false,
-    notifications:Ember.A([]),
-    actions: {
-        ClearToken: function () {
-            $.cookie(AppSettings.CookieName, '', { expires: -1 });
-            window.location='http://'+AppSettings.domain;
-        },
-        StartDrag:  function(){
-            return function (model) {
-                this.set('currentDrag', model);
-            };
-        },
-        OpenLoginModal:function(){
-         Ember.Widgets.ModalComponent.popup({
-              targetObject: this,
-              confirm: "LoginConf",
-             // cancel: "BaseModalCancel",
-             // content: content,
-              contentViewClass:App.LoginModalView,
-              headerText:'Login'
-            });
-        },
-      LoginGoogle:function(){
-        console.log('GOOGLE');
-      },
-      LoginConf:function(){
-
-      }
-
+App.ApplicationController = Ember.Controller.extend(App.PopupOpenerMixin, {
+  currentDrag: null,
+  menuOpen: false,
+  notifications: Ember.A([]),
+  actions: {
+    ClearToken: function() {
+      $.cookie(AppSettings.CookieName, '', {
+        expires: -1
+      });
+      window.location = 'http://' + AppSettings.domain;
     },
-    MouseMove: function (application) {
-        var controller = application;
-         function mouseMove (event) {
-             console.log(controller.get('currentDrag'));
-             console.log(application.get('currentDrag'));
-         }
-         return mouseMove;
+    StartDrag: function() {
+      return function(model) {
+        this.set('currentDrag', model);
+      };
     },
-   SubscribeToContextMenuEvents:function(){
-      Em.subscribe('contextMenu.open',this.get('MouseMove')(this.get('model')));
-   },
-   ToggleContextMenu: function (model) {
-      return {
-        before: function(name, timestamp, event) {
-        },
-        after: function(name, timestamp, event, beforeRet) {
-        }
-      };
-   },
-   SetContextType: function (model) {
-      return {
-        before: function(name, timestamp, event) {
-        },
-        after: function(name, timestamp, event, beforeRet) {
-        }
-      };
-   }
+    OpenLoginModal: function() {
+      Ember.Widgets.ModalComponent.popup({
+        targetObject: this,
+        confirm: "LoginConf",
+        // cancel: "BaseModalCancel",
+        // content: content,
+        contentViewClass: App.LoginModalView,
+        headerText: 'Login'
+      });
+    },
+    LoginFacebook: function() {
+      this.send('Login', 'facebook')
+    },
+    LoginGoogle: function() {
+      this.send('Login', 'google')
+    },
+    LoginGithub: function() {
+      this.send('Login', 'github')
+    },
+    Logintwitter: function() {
+      this.send('Login', 'twitter')
+    },
+    Login: function(provider) {
+      window.location = 'https://' + window.location.host + ':4730/auth/' + provider;
+    }
+
+  },
+  MouseMove: function(application) {
+    var controller = application;
+
+    function mouseMove(event) {
+      console.log(controller.get('currentDrag'));
+      console.log(application.get('currentDrag'));
+    }
+    return mouseMove;
+  },
+  SubscribeToContextMenuEvents: function() {
+    Em.subscribe('contextMenu.open', this.get('MouseMove')(this.get('model')));
+  },
+  ToggleContextMenu: function(model) {
+    return {
+      before: function(name, timestamp, event) {},
+      after: function(name, timestamp, event, beforeRet) {}
+    };
+  },
+  SetContextType: function(model) {
+    return {
+      before: function(name, timestamp, event) {},
+      after: function(name, timestamp, event, beforeRet) {}
+    };
+  }
 });
