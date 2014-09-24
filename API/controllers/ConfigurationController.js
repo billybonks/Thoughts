fa72f1c940fd44e445f10b2fb9c952d88dd96229
Sebatien Stettler <sebastienstettler@gmail.com>
2014-09-24 19:27:18 +0400

    [ENHANCMENT] Improved model framework

--- API/controllers/ConfigurationController.js
+++ API/controllers/ConfigurationController.js
@@ -3,129 +3,130 @@ var Promise = require('./../lib/promise')
 var error = require('./../lib/Errors.js').reject;
 var model = require('./../models/configuration')();
 module.exports = function() {
-    'use strict';
-    /* ========================================================================================================
-     *
-     * Class Setup - Keep in alphabetical order
-     *
-     * ===================================================================================================== */
-    function ConfigurationController() {}
+  'use strict';
+  /* ========================================================================================================
+   *
+   * Class Setup - Keep in alphabetical order
+   *
+   * ===================================================================================================== */
+  function ConfigurationController() {}
 
-    ConfigurationController.prototype = new controller();
+  ConfigurationController.prototype = new controller();
 
-    /**
-     * Gets configuration objects based on specified ids
-     * @param {ids} Array configuration id
-     * @return
-     */
-    ConfigurationController.prototype.getConfiguration = function(ids) {
-        return Promise.call(this, function(resolve, reject) {
-            var ret = {};
-            var context = this;
-            var counter = 0;
-            for (var i = 0; i < ids.length; i++) {
-                var query = ['Start configuration=node(' + ids[i] + ')',
-                    'Match configuration-[c:Configures]->card',
-                    'Match configuration-[f:Targets]->target',
-                    'Return card,configuration,target'
-                ];
-                this.executeQueryRSVP(query.join('\n'), {}).then(function(results) {
-                    for (var i = 0; i < results.length; i++) {
-                        var config = context.FormatNeo4jObject(results[i]);
-                        ret[config.id] = config;
-                    }
-                    counter++;
-                    if (counter === ids.length) {
-                        resolve(ret);
-                    }
-                }, error(reject));
-            }
-        });
-    }
+  /**
+   * Gets configuration objects based on specified ids
+   * @param {ids} Array configuration id
+   * @return
+   */
+  ConfigurationController.prototype.getConfiguration = function(ids) {
+    return Promise.call(this, function(resolve, reject) {
+      var ret = {};
+      var context = this;
+      var counter = 0;
+      for (var i = 0; i < ids.length; i++) {
+        var query = ['Start node=node(' + ids[i] + ')',
+          'Match node-[c:Configures]->configures',
+          'Match node-[f:Targets]->for',
+          'Return configures,node,for'
+        ];
+        this.executeQueryRSVP(query.join('\n'), {}).then(function(results) {
+          for (var i = 0; i < results.length; i++) {
+            var configuration = new model();
+            configuration.parse(results[i]);
+            ret[config.id] = configuration.getJSON();
+          }
+          counter++;
+          if (counter === ids.length) {
+            resolve(ret);
+          }
+        }, error(reject));
+      }
+    });
+  }
 
-    /**
-     * Gets configuration objects based on card id
-     * @param {cardId} Object cardId
-     * @return
-     */
-    ConfigurationController.prototype.getCardConfigurations = function(cardId) {
-        return Promise.call(this, function(resolve, reject) {
-            var context = this;
-            var query = ['Start card=node(' + cardId + ')',
-                'Match configuration-[c:Configures]->card',
-                'Match configuration-[f:Targets]->target',
-                'Return card,configuration,target'
-            ];
-            this.executeQueryRSVP(query.join('\n'), {}).then(function(results) {
-                var configurations = {}
-                for (var i = 0; i < results.length; i++) {
-                    var config = context.FormatNeo4jObject(results[i]);
-                    configurations[config.id] = config;
-                }
-                resolve(configurations)
-            }, error(reject));
-        });
-    }
-
-    /**
-     * Creates configuration node.
-     * @param {targetId} String Target being configured
-     * @param {bindingCardId} String Card context when configuration applies
-     * @param {configuration} Object Configuration data
-     * @return
-     */
-
-    ConfigurationController.prototype.createCardConfiguartion = function(configuration) {
-        return Promise.call(this, function(resolve, reject) {
-            var context = this;
-            this.createNode(configuration.getVectorData(), 'Configuration').then(function(data) {
-                configuration.parse(data[0].node);
-                context.createRelationShip(configuration.get('id'), configuration.get('configures'), 'Configures').then(function() {
-                    context.createRelationShip(configuration.get('id'), configuration.get('for'), 'Targets').then(function() {
-                        resolve(configuration);
-                    }, error(reject))
-                }, error(reject))
-            }, error(reject));
-        });
-    }
-
-    /**
-     * Creates configuration node.
-     * @param {id} String Configuration id to be updated
-     * @param {data} Object Updated values
-     * @return
-     */
-    ConfigurationController.prototype.updateConfiguration = function(id, data) {
-        return Promise.call(this, function(resolve, reject) {
-            var query = ['START configuration=node(' + id + ')',
-                'SET configuration.embedded = {embedded},',
-                'configuration.position = {position}',
-                'RETURN configuration'
-            ];
-            console.log(data);
-            var variableHash = data;
-            this.executeQueryRSVP(query.join('\n'), data).then(function(data) {
-                resolve(data);
-            }, error(reject))
-        });
-    }
-
-    ConfigurationController.prototype.FormatNeo4jObject = function(data) {
-        var target = data.card.id;
-        var f = data.target.id
-        var config = data.configuration
-        return this.FormatObject(config, target, f);
-    }
-
-    ConfigurationController.prototype.FormatObject = function(data, target, f) {
-        return {
-            id: data.id,
-            position: data.data.position,
-            'for': f,
-            embedded: data.data.embedded,
-            configures: target
+  /**
+   * Gets configuration objects based on card id
+   * @param {cardId} Object cardId
+   * @return
+   */
+  ConfigurationController.prototype.getCardConfigurations = function(cardId) {
+    return Promise.call(this, function(resolve, reject) {
+      var context = this;
+      var query = ['Start configures=node(' + cardId + ')',
+        'Match node-[c:Configures]->configures',
+        'Match node-[f:Targets]->for',
+        'Return configures,node,for'
+      ];
+      this.executeQueryRSVP(query.join('\n'), {}).then(function(results) {
+        var configurations = {}
+        for (var i = 0; i < results.length; i++) {
+          var configuration = new model();
+          configuration.parse(results[i]);
+          configurations[config.id] = configuration.getJSON();
         }
-    }
+        resolve(configurations)
+      }, error(reject));
+    });
+  }
 
-    return new ConfigurationController();
+  /**
+   * Creates configuration node.
+   * @param {targetId} String Target being configured
+   * @param {bindingCardId} String Card context when configuration applies
+   * @param {configuration} Object Configuration data
+   * @return
+   */
+
+  ConfigurationController.prototype.createCardConfiguartion = function(configuration) {
+    return Promise.call(this, function(resolve, reject) {
+      var context = this;
+      this.createNode(configuration.getVectorData(), 'Configuration').then(function(data) {
+        configuration.parse(data[0]);
+        context.createRelationShip(configuration.get('id'), configuration.get('configures'), 'Configures').then(function() {
+          context.createRelationShip(configuration.get('id'), configuration.get('for'), 'Targets').then(function() {
+            resolve(configuration);
+          }, error(reject))
+        }, error(reject))
+      }, error(reject));
+    });
+  }
+
+  /**
+   * Creates configuration node.
+   * @param {id} String Configuration id to be updated
+   * @param {data} Object Updated values
+   * @return
+   */
+  ConfigurationController.prototype.updateConfiguration = function(id, data) {
+    return Promise.call(this, function(resolve, reject) {
+      var query = ['START configuration=node(' + id + ')',
+        'SET configuration.embedded = {embedded},',
+        'configuration.position = {position}',
+        'RETURN configuration'
+      ];
+      var variableHash = data;
+      this.executeQueryRSVP(query.join('\n'), data).then(function(data) {
+        resolve(data);
+      }, error(reject))
+    });
+  }
+
+  ConfigurationController.prototype.FormatNeo4jObject = function(data) {
+    var target = data.configures.id;
+    var f = data.for.id
+    var config = data.node
+    return this.FormatObject(config, target, f);
+  }
+
+  ConfigurationController.prototype.FormatObject = function(data, target, f) {
+    return {
+      id: data.id,
+      position: data.data.position,
+      'for': f,
+      embedded: data.data.embedded,
+      configures: target
+    }
+  }
+
+  return new ConfigurationController();
 }
--- API/models/model.js
+++ API/models/model.js
@@ -1,7 +1,7 @@
 module.exports = function() {
 
     function Model() {
-
+        this.relationships = []
     }
 
     Model.prototype.update = function(node) {
@@ -15,8 +15,23 @@ module.exports = function() {
         return ret;
     }
 
-    Model.prototype.parse = function(node) {
-        this.update(this.flatten(node));
+    Model.prototype.parse = function(data) {
+        var vector = data.node;
+        this.update(this.flatten(vector));
+        for (var i = 0; i < this.relationships.length; i++) {
+            relatedVector = data[this.relationships[i].name];
+            if (relatedVector) {
+                var model = this.getRelationshipModel(this.relationships[i].type);
+                model = new model();
+                relatedVector = this.flatten(relatedVector)
+                model.update(relatedVector);
+                this.set(this.relationships[i].name,model)
+            }
+        }
+
+    }
+    Model.prototype.getRelationshipModel = function(type) {
+      return require('./'+type)();
     }
 
     Model.prototype.getVectorData = function(node) {
@@ -26,11 +41,24 @@ module.exports = function() {
     }
 
     Model.prototype.getJSON = function(node) {
-        return this.data;
+        var ret = {};
+        for(var key in this.data){
+          if(this.data[key].getJSON){
+            ret[key] = this.data[key].get('id');
+          }else{
+            ret[key] = this.data[key];
+          }
+        }
+        console.log(ret)
+        return ret;
     }
 
     Model.prototype.get = function(attribute) {
         return this.data[attribute];
+    }
+
+    Model.prototype.set = function(attribute,data) {
+        this.data[attribute] = data;
     }
 
     Model.prototype._clone = function(data) {
@@ -43,7 +71,7 @@ module.exports = function() {
 
     Model.prototype.clearRelationships = function(data) {
         for (var i = 0; i < this.relationships.length; i++) {
-            delete data[this.relationships[i]];
+            delete data[this.relationships[i].name];
         }
         return data;
     }
@@ -58,5 +86,22 @@ module.exports = function() {
         }
         return data;
     }
+
+    Model.prototype.hasMany = function(name, type) {
+        this.relationships.push({
+            name: name,
+            type: type,
+            direction: 'in'
+        })
+    }
+
+    Model.prototype.belongsTo = function(name, type) {
+        this.relationships.push({
+            name: name,
+            type: type,
+            direction: 'out'
+        })
+    }
+
     return Model;
 }
